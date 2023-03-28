360769456bc0bd853c458d68640b9385f30e44d6
[Alexandr] <[afominyh242@gmail.com]>
2023-03-28 17:49:05 +0700

    Получил битлинк введенной ссылки без разбивки на функции.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from pprint import pprint
 
 import requests
 from dotenv import load_dotenv
@@ -11,8 +12,11 @@ headers = {
     'Authorization': f'Bearer {BITLY_TOKEN}',
     'Content-Type': 'application/json',
 }
+user_input_long_url = input('Введите ссылку для укорачивания в формате - https://google.com: ')
+data = f'{{"long_url": "{user_input_long_url}"}}'
 
-response = requests.get('https://api-ssl.bitly.com/v4/user', headers=headers)
+response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
 response.raise_for_status()
 
-print(response.json())
+print(response.json()['link'])
+# print(data)
