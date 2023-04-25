ec44d0199527f9610aaa9309e55475e623a9c51b
[lowwwcode] <[lowwwcode@gmail.com]>
2023-04-26 02:26:49 +0700

    Ввод ссылки в виде аргумента из под командной строки.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from urllib.parse import urlparse
 import requests
 from dotenv import load_dotenv
@@ -54,18 +55,20 @@ def is_bitlink(token, url):
 if __name__ == '__main__':
 
     load_dotenv()
-
     bitly_token = os.getenv('BITLY_TOKEN')
-    user_input = input('Введите ссылку для укорачивания в формате - "https://google.com": ')
 
-    if is_bitlink(bitly_token, user_input):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("link", help="Link to shorten or bitlink", type=str)
+    args_link = parser.parse_args()
+
+    if is_bitlink(bitly_token, args_link.link):
         try:
-            print(f'Всего кликов: {get_clicks_count(bitly_token, user_input)}')
+            print(f'Всего кликов: {get_clicks_count(bitly_token, args_link.link)}')
         except requests.exceptions.HTTPError:
             print('Этот битлинк не зарегистрирован в системе, или вы ввели некорректный адрес ссылки.')
     else:
         try:
-            print(f'Битлинк, {shorten_link(bitly_token, user_input)}')
+            print(f'Битлинк, {shorten_link(bitly_token, args_link.link)}')
         except requests.exceptions.HTTPError:
             print('Вы ввели некорректный формат ссылки или такого адреса не существует, '
                   'ссылка должна начинаться с "https://"')
